7ca41f2bd159941d989adb2fd5fcc48862b59e07
Han Lam <hanlam@phygitallabs.io>
2025-11-11 17:27:42 +0700

    basic setup

---
+++ src/app/thank-you/[id]/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { data } from "@/src/constant/data";
+import { useParams } from "next/navigation";
+
+interface Person {
+  id: string;
+  name: string;
+  type: string;
+  message: string;
+}
+
+const ThankYouPage = () => {
+  const params = useParams();
+  const id = params?.id as string;
+  console.log(id);
+  const person = data.find((item: Person) => item.id === id);
+
+  if (!person) {
+    return (
+      <div>
+        <h1>Person not found</h1>
+        <p>No person found with ID: {id}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Thank You Page</h1>
+      <div>
+        <p>Name: {person.name}</p>
+        <p>Type: {person.type}</p>
+        {person.message && <p>Message: {person.message}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default ThankYouPage;
